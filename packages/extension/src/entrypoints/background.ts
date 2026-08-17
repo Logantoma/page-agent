944a9a3e3170900aabbf7978921c6ea916c4ddc7
Logantoma <jiumingk@gmail.com>
2026-08-17 14:56:00 +0800

    feat(extension): route visual observation capture messages

--- packages/extension/src/entrypoints/background.ts
+++ packages/extension/src/entrypoints/background.ts
@@ -1,5 +1,6 @@
 import { handlePageControlMessage } from '@/agent/RemotePageController.background'
 import { handleTabControlMessage } from '@/agent/TabsController.background'
+import { handleVisualObservationMessage } from '@/agent/VisualObservation.background'
 
 export default defineBackground(() => {
 	console.log('[Background] Service Worker started')
@@ -20,6 +21,8 @@ export default defineBackground(() => {
 			return handleTabControlMessage(message, sender, sendResponse)
 		} else if (message.type === 'PAGE_CONTROL') {
 			return handlePageControlMessage(message, sender, sendResponse)
+		} else if (message.type === 'VISUAL_OBSERVATION_CAPTURE') {
+			return handleVisualObservationMessage(message, sender, sendResponse)
 		} else {
 			sendResponse({ error: 'Unknown message type' })
 			return
